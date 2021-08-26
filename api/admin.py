d96f05ea197cead80cc5f96f31a467e3471b288e
Loriaaa123 <davitloria96@gmail.com>
2021-08-26 11:20:55 +0400

    first commit

---
+++ api/admin.py
@@ -0,0 +1,6 @@
+from api.models import Movie
+from django.contrib import admin
+from .models import Movie, Rating 
+# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Rating)
---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from api.views import MovieViewSet
+from django.contrib import admin
+from django.urls import path
+from rest_framework import routers
+from django.conf.urls import include
+from .views import MovieViewSet, RatingViewSet, UserViewSet
+
+router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
+router.register('users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
